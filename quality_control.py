a05bc698d8241b6caae64e3ea13c0c763001c323
Leah <lobrien20@qub.ac.uk>
2022-08-01 13:51:41 +0100

    improve quality_control.py

--- quality_control.py
+++ quality_control.py
@@ -82,11 +82,50 @@ class dataset: # dataset object with fastq paths and attributes to be added etc.
     def get_sample_names(self):
         sample_paths_without_ext = [".".join(fastq.split(".")[:-1]) for fastq in self.initial_fastq_paths]
         sample_names = [path.split("/")[-1] for path in sample_paths_without_ext]
+        
+        if self.configuration_dict['paired_or_unpaired'] == True:
+            sample_names = self.get_pairs(sample_names)
+       
         return sample_names
 
     def get_fastq_paths(self):
-        fastq_paths = ["%s/%s" % (self.dataset_path, file) for file in os.listdir(self.dataset_path) if file[-6:] == '.fastq' or file[-3:] == '.fq']
+        if self.configuration_dict['gzip_compressed'] == 'Y':
+            fastq_paths = ["%s/%s" % (self.dataset_path, file) for file in os.listdir(self.dataset_path) if file[-9:] == '.fastq.gz' or file[-6:] == '.fq.gz']
+        else:
+            fastq_paths = ["%s/%s" % (self.dataset_path, file) for file in os.listdir(self.dataset_path) if file[-9:] == '.fastq' or file[-6:] == '.fq']
         return fastq_paths
+
+    def get_pairs(self, list_of_fastqs):
+        forward_pair = self.configuration_dict['forward_pair']
+        backward_pair = self.configuration_dict['backward_pair']
+        unique_fastqs_dict = []
+        for fastq in self.sample_names:
+            if forward_pair in fastq:
+                
+                fastq_no_ext = fastq.replace(forward_pair, "")
+            
+            else:
+                
+                fastq_no_ext = fastq.replace(backward_pair, "")
+            
+            if fastq_no_ext not in unique_fastqs_dict.keys():
+                
+                unique_fastqs_dict[fastq_no_ext] =  fastq
+
+            else:
+
+                if forward_pair in fastq:
+
+                    fastq_names = [fastq, unique_fastqs_dict[fastq_no_ext]]
+
+                else:
+
+                    fastq_names = [unique_fastqs_dict[fastq_no_ext], fastq]
+
+                unique_fastqs_dict[fastq_no_ext] = fastq_names
+        
+        return unique_fastqs
+
 
     def run_fastqc_and_multiqc(self, fastq_files):
         initial_fastqc_directory = f"{self.configuration_dict['output_directory']}/initial_fastqc_results"
@@ -104,11 +143,43 @@ class dataset: # dataset object with fastq paths and attributes to be added etc.
         subprocess.call(multiqc_args)
 
    
-    def run_trimming(self, fastq_files):
+    def run_trimming(self):
         trimming_directory = f"{self.configuration_dict['output_directory']}/trimmed_fastqs"
         os.mkdir(trimming_directory)
-        trim_galore_args = ['trim_galore', ]
+        if self.configuration_dict['paired_or_unpaired'] == 'Y':
+            for sample_name,fwd_and_bck in self.sample_names.items():
+                forward_sample = f"{self.dataset_path}/{fwd_and_bck[0]}"
+                backward_sample = f"{self.dataset_path}/{fwd_and_bck[1]}"
 
+                trim_galore_args = ['trim_galore', '-q', self.configuration_dict['trim_phred_quality'], self.configuration_dict['minimum_read_length'], '--trim-n', '--cores', self.configuration_dict['threads'], '--output_dir', trimming_directory, '--paired', forward_sample, backward_sample]
+                subprocess.call(trim_galore_args)
+        # trim_galore -q 20 --gzip --paired --length 50 --trim-n --output_dir --cores
 
+    def run_merging(self):
+        merging_directory = f"{self.configuration_dict['output_directory']}/merged_fastqs"
+        os.mkdir(merging_directory)
+        for sample_name,fwd_and_bck in self.sample_names.items():
+                
+                forward_sample = f"{self.dataset_path}/{fwd_and_bck[0]}"
+                backward_sample = f"{self.dataset_path}/{fwd_and_bck[1]}"
+                merged_fastq_path = f"{merging_directory}/merged_{sample_name}"
+
+                merge_args = ['NGmerge', '-m', self.configuration_dict['minimum_ngmerge_overlap'], '-p', self.configuration_dict['perc_mismatches_allowed_in_overlap'], '-1', forward_sample, '-2', backward_sample, '-o', merged_fastq_path]
+                subprocess.call(merge_args)
+
+    def run_bowtie_alignment(self, fastq_paths):
+        
+        bowtie_directory = f"{self.configuration_dict['results_directory']}/bowtie_alignment_directory"
+        os.mkdir(bowtie_directory)
+        summary_directory = f"{bowtie_directory}/results_summaries"
+        os.mkdir(summary_directory)
+        
+        for fastq in fastq_paths:
+            
+            fastq_name = fastq.split("/")[-1]
+            unaligned_reads_path = f"{bowtie_directory}/bowtie_unaligned_{fastq_name}"
+            sum_path = f"{summary_directory}/{fastq.split('.')[0]}_bowtie_sum.txt"
+            bowtie_args = ['bowtie2', '-x', self.configuration_dict['bowtie_host_indice_directory'], '-U', fastq, '--very-sensitive', '-p', self.configuration_dict['threads'], '>', unaligned_reads_path, '2>', sum_path]
+            subprocess.call(bowtie_args)
 
 workflow_manager(sys.argv[1])
